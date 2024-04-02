0cc0484df4d6aeaa74dbdba54bc047a73319e224
Ngo Manh Thong <ngomanhthong0812>
2024-04-02 11:44:29 +0700

    update table detail

--- app/src/main/java/com/example/kiotviet_fake/activities/TableDetailActivity.java
+++ app/src/main/java/com/example/kiotviet_fake/activities/TableDetailActivity.java
@@ -7,8 +7,11 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.kiotviet_fake.R;
 import com.example.kiotviet_fake.adapters.ProductAdapter;
@@ -19,12 +22,27 @@ import com.example.kiotviet_fake.models.Table;
 import java.util.ArrayList;
 
 public class TableDetailActivity extends AppCompatActivity {
+    ImageView btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_detail);
         initView();
+        addControl();
+        btnClick();
+    }
+    public void btnClick(){
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TableDetailActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void addControl(){
+        btnCancel = (ImageView) findViewById(R.id.btnCancel);
     }
 
     public void initView() {
@@ -39,8 +57,12 @@ public class TableDetailActivity extends AppCompatActivity {
         arrayList.add(new Product("BÀN 3", "19,000", "2"));
         arrayList.add(new Product("BÀN 3", "18,000", "2"));
         arrayList.add(new Product("BÀN 3", "18,000", "2"));
+        arrayList.add(new Product("BÀN 3", "18,000", "2"));
+        arrayList.add(new Product("BÀN 3", "18,000", "2"));
+        arrayList.add(new Product("BÀN 3", "18,000", "2"));
+        arrayList.add(new Product("BÀN 3", "18,000", "2"));
 
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),DividerItemDecoration.VERTICAL);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
         Drawable drawable = ContextCompat.getDrawable(getApplicationContext(), R.drawable.divider);
         dividerItemDecoration.setDrawable(drawable);
         recyclerView.addItemDecoration(dividerItemDecoration);
