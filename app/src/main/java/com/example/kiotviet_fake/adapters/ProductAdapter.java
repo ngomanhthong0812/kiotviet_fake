5c9951486866ddd710023f4eadd08d3aa046b47c
quangk22 <quangcute504@gmail.com>
2024-04-08 23:22:41 +0700

    quang

--- app/src/main/java/com/example/kiotviet_fake/adapters/ProductAdapter.java
+++ app/src/main/java/com/example/kiotviet_fake/adapters/ProductAdapter.java
@@ -20,11 +20,18 @@ import com.example.kiotviet_fake.R;
 import com.example.kiotviet_fake.activities.LoginActivity;
 import com.example.kiotviet_fake.activities.MainActivity;
 import com.example.kiotviet_fake.activities.ProductDetailActivity;
+import com.example.kiotviet_fake.database.deleteItems.DeleteItemOfOrderAPI;
+import com.example.kiotviet_fake.database.deleteItems.DeleteItemOfOrderService;
 import com.example.kiotviet_fake.models.Order;
 import com.example.kiotviet_fake.models.Product;
 import com.example.kiotviet_fake.session.SessionManager;
 
+import java.text.ParseException;
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.viewHolder> {
     ArrayList<Product> products;
@@ -65,7 +72,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.viewHold
         holder.item.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (count[0] > 0) {
+                if (!product.getIdProductItem().equals("") && count[0] > 0) {
                     holder.countQuanity.setVisibility(View.VISIBLE);
                     holder.imgCheck.setVisibility(View.VISIBLE);
                     product.setQuantityOrder(1); // gán số lượng 1 khi chọn vào sản phẩm
@@ -126,16 +133,44 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.viewHold
                             intent.putExtra("product_id", product.getId());
                             intent.putExtra("product_name", product.getName());
                             intent.putExtra("product_price", product.getPrice());
-
+                            intent.putExtra("product_quantity", product.getQuantity());
+                            System.out.println("test SLL produxt:" + product.getQuantity());
                             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // Thêm cờ FLAG_ACTIVITY_NEW_TASK
                             context.startActivity(intent); // Sử dụng context để khởi chạy Intent
                         }
                         if (item.getItemId() == R.id.action_delete) {
                             // Xử lý khi người dùng chọn sửa
                             Log.e("TAG", "onMenuItemClick: " + product.getId());
+                            DeleteOrderProductItem("11168851", "60-dayfreetrial");
                             return true;
                         }
                         return false;
+                    }
+
+                    private void DeleteOrderProductItem(String username, String password)  {
+
+                        DeleteItemOfOrderService service =  DeleteItemOfOrderAPI.createService(username, password);
+                        Call<String> call = service.deleteItemOfOrder(product.id);
+                        call.enqueue(new Callback<String>() {
+                            @Override
+                            public void onResponse(Call<String> call, Response<String> response) {
+                                if (response.isSuccessful()) {
+                                    // Xử lý phản hồi thành công từ API nếu cần
+                                    Log.d("DeleteItemOfOrder", "Xóa sản phẩm thành công id  : " + product.getId());
+
+                                    notifyDataSetChanged();
+
+                                } else {
+                                    // Xử lý phản hồi lỗi từ API nếu cần
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<String> call, Throwable t) {
+
+                            }
+                        });
+
                     }
                 });
                 popupMenu.show();
