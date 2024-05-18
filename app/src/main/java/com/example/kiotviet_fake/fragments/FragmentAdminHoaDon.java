2f440b8208c21549f9485874dd72b9413761cefb
quangk22 <quangcute504@gmail.com>
2024-05-18 22:55:22 +0700

    add

--- app/src/main/java/com/example/kiotviet_fake/fragments/FragmentAdminHoaDon.java
+++ app/src/main/java/com/example/kiotviet_fake/fragments/FragmentAdminHoaDon.java
@@ -2,6 +2,7 @@ package com.example.kiotviet_fake.fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -10,13 +11,17 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -36,7 +41,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 import retrofit2.Call;
@@ -47,8 +56,10 @@ public class FragmentAdminHoaDon extends Fragment {
     private ArrayList<Bill_Admin> Bill_AdminArayList;
     private BillAdminAdapter billAdminAdapter;
     private RecyclerView recyclerView_bill;
-    private TextView totalAmountTextView;
+    private TextView totalAmountTextView,dayMonthYearr,dayMonthYearr2;
+    LinearLayout xemTheoLich;
     String id_shop;
+
     public FragmentAdminHoaDon() {
     }
 
@@ -62,6 +73,10 @@ public class FragmentAdminHoaDon extends Fragment {
 
         totalAmountTextView = view.findViewById(R.id.total_all_bills);
 
+        xemTheoLich = view.findViewById(R.id.xemTheoLich);
+        dayMonthYearr =view.findViewById(R.id.dayMonthYearr);
+        dayMonthYearr2 = view.findViewById(R.id.dayMonthYearr2);
+
         Bill_AdminArayList = new ArrayList<>();
         billAdminAdapter = new BillAdminAdapter(Bill_AdminArayList, getContext());
         recyclerView_bill.setAdapter(billAdminAdapter);
@@ -74,16 +89,78 @@ public class FragmentAdminHoaDon extends Fragment {
         SharedPreferences sharedPreferences = requireContext().getSharedPreferences("user", Context.MODE_PRIVATE);
         id_shop = sharedPreferences.getString("shop_id","");
         System.out.println("test id_shop : "+ id_shop);
+
+        xemTheoLich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showOptionsDialog();
+            }
+        });
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        LoadDataHoaDon(); // Di chuyển hàm gọi LoadDataHoaDon() sau khi adapter được thiết lập
+        LoadDataHoaDon(0,0,0); // Di chuyển hàm gọi LoadDataHoaDon() sau khi adapter được thiết lập
         addDividerToRecyclerView();
-    }
 
+    }
+    private void showOptionsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setTitle("Chọn kiểu xem");
+
+        String[] options = {"Xem theo ngày", "Xem từ ngày đến ngày", "Xem tất cả bill"};
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        showDayPickerDialog();
+                        break;
+                    case 1:
+//                        showDayToDayPickerDialog();
+                        break;
+                    case 2:
+                        dayMonthYearr.setText("Xem tất cả");
+                        dayMonthYearr2.setText("Xem tất cả");
+                        LoadDataHoaDon(0,0,0);
+
+                        break;
+                }
+            }
+
+
+
+
+        });
+
+        builder.show();
+    }
+    private void showDayPickerDialog() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Không cần thực hiện gì ở đây vì bạn chỉ muốn chọn tháng và năm
+                        dayMonthYearr.setText(String.format("%02d/%02d/%d", dayOfMonth,month + 1, year));
+                        dayMonthYearr2.setText(String.format("%02d/%02d/%d", dayOfMonth,month + 1, year));
+                        LoadDataHoaDon(year,  month + 1, dayOfMonth);
+
+                    }
+                }, year, month, 1); // 1: ngày mặc định sẽ hiển thị, có thể là bất kỳ ngày nào vì nó sẽ bị ẩn đi
+
+        // Ẩn bảng chọn ngày và hiển thị spinner cho tháng và năm
+        datePickerDialog.getDatePicker().setSpinnersShown(true);
+        datePickerDialog.getDatePicker().setCalendarViewShown(false);
+
+        datePickerDialog.show();
+    }
     private void addDividerToRecyclerView() {
         // Thêm dường viền vào RecyclerView
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView_bill.getContext(), DividerItemDecoration.VERTICAL);
@@ -92,7 +169,7 @@ public class FragmentAdminHoaDon extends Fragment {
         recyclerView_bill.addItemDecoration(dividerItemDecoration);
     }
 
-    public void LoadDataHoaDon() {
+    public void LoadDataHoaDon(int year, int month, int dayOfMonth) {
         Bills_Admin billAdmin =  RetrofitClient.getRetrofitInstance("11168851", "60-dayfreetrial").create(Bills_Admin.class);
 
         Call<String> call = billAdmin.getBills_Admin(id_shop);
@@ -107,6 +184,11 @@ public class FragmentAdminHoaDon extends Fragment {
                         JSONArray jsonArray = new JSONArray(response.body());
                         Bill_AdminArayList.clear();
                         double totalAmount = 0;
+                        // Định dạng của chuỗi datetime
+                        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        SimpleDateFormat outputYearFormat = new SimpleDateFormat("yyyy");
+                        SimpleDateFormat outputMonthFormat = new SimpleDateFormat("MM");
+                        SimpleDateFormat outputDatFormat = new SimpleDateFormat("dd");
                         for (int i = 0; i < jsonArray.length(); i++) {
                             JSONObject jsonObject = jsonArray.getJSONObject(i);
                             int id_bill = jsonObject.getInt("id_bill");
@@ -120,16 +202,34 @@ public class FragmentAdminHoaDon extends Fragment {
                             String name_user = jsonObject.getString("name_user");
                             String shop_id = jsonObject.getString("id_shop");
 
+                            // Chuyển đổi chuỗi datetime thành đối tượng Date
+                            Date datetime = inputFormat.parse(dateTime_end);
+                            // Lấy năm và tháng từ đối tượng Date
+                            String Year = outputYearFormat.format(datetime);
+                            String Month = outputMonthFormat.format(datetime);
+                            String Day = outputDatFormat.format(datetime);
 
 
-                        if(id_shop.equals(shop_id)){
-                            System.out.println("test id_shop 1: "+ id_shop);
-                            System.out.println("test id_shop 2: "+ shop_id);
-                            totalAmount += total_price;
-                            Bill_Admin billsAdmin = new  Bill_Admin(id_bill, dateTime, dateTime_end, code, table_id, user_id, total_price, name_user,shop_id);
-                            Bill_AdminArayList.add(billsAdmin);
+                            int billYear = Integer.parseInt(Year);
+                            int billMonth = Integer.parseInt(Month);
+                            int billDay = Integer.parseInt(Day);
 
-                        }
+                            if(id_shop.equals(shop_id )){
+                                if(year != 0 && month != 0 && dayOfMonth != 0){
+
+                                    if (billYear == year && billMonth == month && billDay == dayOfMonth){
+                                        totalAmount += total_price;
+                                        Bill_Admin billsAdmin = new  Bill_Admin(id_bill, dateTime, dateTime_end, code, table_id, user_id, total_price, name_user,shop_id);
+                                        Bill_AdminArayList.add(billsAdmin);
+                                    }
+                                }
+                                else{
+                                    totalAmount += total_price;
+                                    Bill_Admin billsAdmin = new  Bill_Admin(id_bill, dateTime, dateTime_end, code, table_id, user_id, total_price, name_user,shop_id);
+                                    Bill_AdminArayList.add(billsAdmin);
+                                }
+
+                            }
 
 
                         }
@@ -139,6 +239,8 @@ public class FragmentAdminHoaDon extends Fragment {
                         billAdminAdapter.notifyDataSetChanged();
                     } catch (JSONException e) {
                         e.printStackTrace();
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
                     }
                 } else {
 
--- app/src/main/java/com/example/kiotviet_fake/fragments/FragmentAdminTongQuan.java
+++ app/src/main/java/com/example/kiotviet_fake/fragments/FragmentAdminTongQuan.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.example.kiotviet_fake.R;
@@ -36,8 +37,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -75,8 +79,7 @@ public class FragmentAdminTongQuan extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        CountQuantityAndTotalBills();
-        GetRevenue();
+        GetRevenueCaNam();
         OnClickButton();
         setupBarChart(view);
     }
@@ -94,7 +97,7 @@ public class FragmentAdminTongQuan extends Fragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
         builder.setTitle("Chọn kiểu xem");
 
-        String[] options = {"Xem theo tháng", "Xem tất cả năm"};
+        String[] options = {"Xem theo tháng", "Xem cả năm"};
 
         builder.setItems(options, new DialogInterface.OnClickListener() {
             @Override
@@ -104,33 +107,20 @@ public class FragmentAdminTongQuan extends Fragment {
                         showMonthPickerDialog();
                         break;
                     case 1:
-                        // showYearPickerDialog();
+                         showYearPickerDialog();
                         break;
                 }
             }
+
+
         });
 
         builder.show();
     }
-
-    private void showMonthPickerDialog() {
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(),
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        selectedDateText.setText(String.format("%02d/%02d/%d", dayOfMonth, month + 1, year));
-//                        updateDataAndChart(year, month + 1);
-                    }
-                }, year, month, 1);
-
-        datePickerDialog.show();
+    private void showYearPickerDialog() {
+        GetRevenueCaNam();
     }
-
-    private void GetRevenue() {
+    private void GetRevenueCaNam() {
         GetRevenue getRevenue = RetrofitClient.getRetrofitInstance("11168851", "60-dayfreetrial").create(GetRevenue.class);
 
         Call<String> call = getRevenue.GetRevenue_Admin(id_shop);
@@ -139,6 +129,91 @@ public class FragmentAdminTongQuan extends Fragment {
             public void onResponse(Call<String> call, Response<String> response) {
                 if (response.isSuccessful()) {
                     try {
+                        // Xóa dữ liệu cũ trong entries trước khi thêm dữ liệu mới
+                        entries.clear();
+
+                        JSONArray jsonArray = new JSONArray(response.body());
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+//                            String year = jsonObject.getString("year");
+                            String Month = jsonObject.getString("Month");
+                            Double Total_Revenue = jsonObject.getDouble("TotalRevenue");
+
+                                int monthValue = Integer.parseInt(Month);
+                                float Revenue = Total_Revenue.floatValue();
+                                entries.add(new BarEntry(monthValue, Revenue));
+
+                        }
+                        selectedDateText.setText("Xem cả năm");
+                        CountQuantityAndTotalBills(0,0);
+
+                        setupBarChart(getView()); // Cập nhật biểu đồ với dữ liệu mới
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+            }
+        });
+    }
+    private void showMonthPickerDialog() {
+        View dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.month_year_picker_dialog, null);
+        NumberPicker monthPicker = dialogView.findViewById(R.id.monthPicker);
+        NumberPicker yearPicker = dialogView.findViewById(R.id.yearPicker);
+
+        // Thiết lập giá trị cho NumberPickers
+        monthPicker.setMinValue(1);
+        monthPicker.setMaxValue(12);
+        monthPicker.setValue(Calendar.getInstance().get(Calendar.MONTH) + 1); // Tháng hiện tại
+        monthPicker.setWrapSelectorWheel(true); // Cho phép lặp lại giá trị
+
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        yearPicker.setMinValue(currentYear - 100); // 100 năm trước
+        yearPicker.setMaxValue(currentYear + 100); // 100 năm sau
+        yearPicker.setValue(currentYear); // Năm hiện tại
+        yearPicker.setWrapSelectorWheel(true); // Cho phép lặp lại giá trị
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        builder.setView(dialogView);
+        builder.setTitle("Chọn tháng và năm");
+
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int selectedMonth = monthPicker.getValue();
+                int selectedYear = yearPicker.getValue();
+
+                // Xử lý logic với tháng và năm đã chọn
+                selectedDateText.setText(String.format("%02d/%d", selectedMonth, selectedYear));
+                // Gọi phương thức để truy vấn dữ liệu doanh thu cho tháng được chọn
+                CountQuantityAndTotalBills(selectedMonth ,selectedYear);
+                GetRevenue(selectedMonth, selectedYear);
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void GetRevenue(int selectedMonth, int selectedYear) {
+
+        GetRevenue getRevenue = RetrofitClient.getRetrofitInstance("11168851", "60-dayfreetrial").create(GetRevenue.class);
+
+        Call<String> call = getRevenue.GetRevenue_Admin(id_shop);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()) {
+                    try {
+                        // Xóa dữ liệu cũ trong entries trước khi thêm dữ liệu mới
+                        entries.clear();
+
                         JSONArray jsonArray = new JSONArray(response.body());
                         for (int i = 0; i < jsonArray.length(); i++) {
                             JSONObject jsonObject = jsonArray.getJSONObject(i);
@@ -146,12 +221,23 @@ public class FragmentAdminTongQuan extends Fragment {
                             String year = jsonObject.getString("year");
                             String Month = jsonObject.getString("Month");
                             Double Total_Revenue = jsonObject.getDouble("TotalRevenue");
+                            if(year.equals(String.valueOf(selectedYear)) && Month.equals(String.valueOf(selectedMonth))){
+                                int monthValue = Integer.parseInt(Month);
+                                float Revenue = Total_Revenue.floatValue();
+                                entries.add(new BarEntry(monthValue, Revenue));
 
-                            int monthValue = Integer.parseInt(Month);
-                            float Revenue = Total_Revenue.floatValue();
-                            entries.add(new BarEntry(monthValue, Revenue));
+                                if(Total_Revenue.equals(null)){
+                                    totalRevenue.setText(0);
+                                }else {
+                                    String formattedTotalAmount = NumberFormat.getNumberInstance(Locale.getDefault()).format(Total_Revenue);
+                                    totalRevenue.setText(formattedTotalAmount);
+                                }
+
+                            }
+
                         }
-                        setupBarChart(getView());
+
+                        setupBarChart(getView()); // Cập nhật biểu đồ với dữ liệu mới
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -164,7 +250,7 @@ public class FragmentAdminTongQuan extends Fragment {
         });
     }
 
-    private void CountQuantityAndTotalBills() {
+    private void CountQuantityAndTotalBills(int month , int year) {
         Bills_Admin billAdmin = RetrofitClient.getRetrofitInstance("11168851", "60-dayfreetrial").create(Bills_Admin.class);
 
         Call<String> call = billAdmin.getBills_Admin(id_shop);
@@ -176,18 +262,55 @@ public class FragmentAdminTongQuan extends Fragment {
                         JSONArray jsonArray = new JSONArray(response.body());
                         double totalAmount = 0;
                         int countBill = 0;
+                        boolean hasBills = false; // Biến kiểm tra xem có hóa đơn nào được tìm thấy không
+
+                        // Định dạng của chuỗi datetime
+                        SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        SimpleDateFormat outputYearFormat = new SimpleDateFormat("yyyy");
+                        SimpleDateFormat outputMonthFormat = new SimpleDateFormat("MM");
+
+
                         for (int i = 0; i < jsonArray.length(); i++) {
                             JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String dateTime_end = jsonObject.getString("dateTimeEnd");
                             double total_price = jsonObject.getDouble("total_price");
-                            totalAmount += total_price;
-                            countBill++;
+
+
+
+                            // Chuyển đổi chuỗi datetime thành đối tượng Date
+                            Date datetime = inputFormat.parse(dateTime_end);
+                            // Lấy năm và tháng từ đối tượng Date
+                            String Year = outputYearFormat.format(datetime);
+                            String Month = outputMonthFormat.format(datetime);
+
+                            int billYear = Integer.parseInt(Year);
+                            int billMonth = Integer.parseInt(Month);
+                            if (year != 0 && month != 0) { // Nếu đang xem theo một tháng cụ thể
+                                if (billYear == year && billMonth == month) {
+                                    totalAmount += total_price;
+                                    countBill++;
+                                    hasBills = true;
+                                }
+                            } else { // Nếu đang xem cả năm
+                                totalAmount += total_price;
+                                countBill++;
+                                hasBills = true;
+                            }
                         }
+
+                        if (!hasBills) { // Nếu không có hóa đơn nào được tìm thấy
+                            totalAmount = 0; // Đặt tổng số tiền thành 0
+                        }
+
                         String formattedTotalAmount = NumberFormat.getNumberInstance(Locale.getDefault()).format(totalAmount);
-                        totalRevenue.setText(formattedTotalAmount + " VND");
+                        totalRevenue.setText(formattedTotalAmount);
+
                         String count_Bill = NumberFormat.getNumberInstance(Locale.getDefault()).format(countBill);
                         totalBill.setText(count_Bill + " hóa đơn");
                     } catch (JSONException e) {
                         e.printStackTrace();
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
                     }
                 }
             }
